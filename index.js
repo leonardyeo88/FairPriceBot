7c1f72897f9de912b3e91446d068666f96f35658
leonardyeo88 <leonardyeo88@gmail.com>
2016-09-10 20:59:28 +0800

    updates

--- index.js
+++ index.js
@@ -31,15 +31,13 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
             var temp = event.message.text;
             temp = temp.toLowerCase();
-            history.push(temp);
-            var output = "";
 
-            if(temp.includes("cake") && history.length == 0){
-               sendMessage(event.sender.id, {text: "Sure!. Here are some cakes that I recommend!"});
-               sendMessage(event.sender.id, {text: "List: Lemon Cake, Chocolate Cake, Tiramisu, Lava Cake, Matcha Cake, Fruit Cake, Strawberry Cake"});
+            if(temp.includes('cake')){
                sendMessage(event.sender.id, {text: "Which one do you like?"});
+               sendMessage(event.sender.id, {text: "List: Lemon Cake, Chocolate Cake, Tiramisu, Lava Cake, Matcha Cake, Fruit Cake, Strawberry Cake"});
+               sendMessage(event.sender.id, {text: "Sure!. Here are some cakes that I recommend!"});
             }
-            if(temp.includes("tiramisu")){
+            if(temp.includes('tiramisu')){
                 sendMessage(event.sender.id, {text: "These are the ingredients you need for the Tiramisu cake, 3 cups of strong black coffee, preferably espresso, cooled. 3 tbsp caster sugar. 6 tbsp Amaretto liqueur.2 eggs, separated.250g/8¾oz mascarpone cheese.250ml/8¾ fl oz whipped cream.cocoa powder, to dust.1 packet of Savoiardi (sponge lady finger biscuits)" });
                 sendMessage(event.sender.id, {text: "Enter 'add to cart' and the item number of the ingredients you would like to order. If you would like more than one item, enter more and the item number. or else 'confirm order' to check out"});
 
@@ -61,23 +59,25 @@ app.post('/webhook', function (req, res) {
                                     }, {
                                     "type": "postback",
                                     "title": "I like this",
-                                    "payload": "User " + recipientId + " likes tiramisu " + imageUrl,
+                                    "payload": "User " + " likes tiramisu " + imageUrl,
                                 }]
                             }]
                         }
                     }
                 };
 
+                sendMessage(event.sender.id, message);
+
 
     
             }
-            if(temp.includes("confirm order")){
+            if(temp.includes('confirm order')){
                 sendMessage(event.sender.id, {text: "Would you like for self-collection or home delivery"});
             }
-            if(temp.includes("collection")){
+            if(temp.includes('collection')){
                 sendMessage(event.sender.id, {text: "Great! Your order is being processed by Maria and will be ready for pick-up in 10 minutes"});
             }
-            if(temp.includes("thanks")){
+            if(temp.includes('thanks')){
                sendMessage(event.sender.id, {text: "Sure Thing! Good luck with your baking and happy birthday to John!"});
             }
             
